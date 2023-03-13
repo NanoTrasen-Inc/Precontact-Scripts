3b2923a91e231d86faa79b05846dcb094a80252c
NanoTrasen-Inc <nano@nanotrasen.org>
2023-03-13 04:54:17 -0500

    Update Sneaker GCI

--- Sneaker GCI/Start Sneaker GCI.bat
+++ Sneaker GCI/Start Sneaker GCI.bat
@@ -7,7 +7,7 @@ set script_dir=%~dp0
 set script_dir=%script_dir:~0,-1%
 
 :start
-start "Sneaker GCI" /wait /belownormal "%script_dir%\sneaker-gci.exe" --bind 0.0.0.0:8080 --config "%script_dir%\config.json"
+start "Sneaker GCI" /wait /belownormal "%script_dir%\sneaker.exe" --bind 0.0.0.0:7788 --config "%script_dir%\config.json"
 timeout 5
 
 goto :start
