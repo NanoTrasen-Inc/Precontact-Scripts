1743481b6d10f57522c152a48cd998add9ba4759
NanoTrasen-Inc <nano@nanotrasen.org>
2023-03-12 23:28:07 -0500

    Add comment

--- Start DCS Servers/Start Server 1.bat
+++ Start DCS Servers/Start Server 1.bat
@@ -5,6 +5,7 @@ set script_dir=%script_dir:~0,-1%
 
 :: For process affinity/core restriction, see https://ss64.com/nt/start.html and https://stackoverflow.com/questions/7759948/set-affinity-with-start-affinity-command-on-windows-7
 
+:: Start process minimized, and with 'abovenormal' priority level
 echo Starting DCS Server 1...
 start "DCS.server1" /d "C:\Program Files\Eagle Dynamics\DCS World OpenBeta Server" /min /abovenormal "C:\Program Files\Eagle Dynamics\DCS World OpenBeta Server\bin\DCS.exe" --server --norender -w DCS.server1
 
