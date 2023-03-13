d2d5487dc4ad2f8f7a327ff01f1baa6a087fdc12
NanoTrasen-Inc <nano@nanotrasen.org>
2023-03-13 01:02:42 -0500

    Add LSO bot

---
+++ LSO Bot/Start LSO Bots (sample).bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal
+setlocal EnableExtensions
+setlocal EnableDelayedExpansion
+
+set script_dir=%~dp0
+set script_dir=%script_dir:~0,-1%
+
+taskkill /f /im lso.exe
+
+start "LSO Bot #1" /min /belownormal "%script_dir%\lso.exe" run -o gen --uri http://127.0.0.1:50011 --discord-webhook <Discord webhook URL>
+start "LSO Bot #2" /min /belownormal "%script_dir%\lso.exe" run -o gen --uri http://127.0.0.1:50021 --discord-webhook <Discord webhook URL>
+start "LSO Bot #3" /min /belownormal "%script_dir%\lso.exe" run -o gen --uri http://127.0.0.1:50031 --discord-webhook <Discord webhook URL>
