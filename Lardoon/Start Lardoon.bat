51ece222343fa5870dd22086f429b11cbd7d9fa9
NanoTrasen-Inc <nano@nanotrasen.org>
2023-03-13 05:04:42 -0500

    oops

--- Lardoon/Start Lardoon.bat
+++ Lardoon/Start Lardoon.bat
@@ -7,7 +7,7 @@ set script_dir=%~dp0
 set script_dir=%script_dir:~0,-1%
 
 :start
-start "Lardoon" /wait /belownormal "%script_dir%\lardoon.exe" --bind 0.0.0.0:3883
+start "Lardoon" /wait /belownormal "%script_dir%\lardoon.exe" serve --bind 0.0.0.0:3883
 timeout 5
 
 goto :start
