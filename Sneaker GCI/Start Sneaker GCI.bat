86bd0ae0c14eb0fedec249f0fad736c6561e6152
NanoTrasen-Inc <nano@nanotrasen.org>
2023-03-13 00:57:19 -0500

    Remove working directory requirement

--- Sneaker GCI/Start Sneaker GCI.bat
+++ Sneaker GCI/Start Sneaker GCI.bat
@@ -1,7 +1,13 @@
 @echo off
+setlocal
+setlocal EnableExtensions
+setlocal EnableDelayedExpansion
+
+set script_dir=%~dp0
+set script_dir=%script_dir:~0,-1%
 
 :start
-start "Sneaker GCI" /wait /belownormal sneaker-gci.exe --bind 0.0.0.0:8080 --config config.json
+start "Sneaker GCI" /wait /belownormal "%script_dir%\sneaker-gci.exe" --bind 0.0.0.0:8080 --config "%script_dir%\config.json"
 timeout 5
 
 goto :start
