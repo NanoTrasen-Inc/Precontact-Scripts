a781427299375fabce76c599d2c726c96e16f62d
NanoTrasen-Inc <nano@nanotrasen.org>
2023-03-13 07:38:47 -0500

    Remove prune

--- Lardoon/Prune.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-setlocal EnableExtensions
-setlocal EnableDelayedExpansion
-
-set script_dir=%~dp0
-set script_dir=%script_dir:~0,-1%
-
-start "Lardoon" /d "%script_dir%" /min /low "%script_dir%\lardoon.exe" prune --no-dry-run
