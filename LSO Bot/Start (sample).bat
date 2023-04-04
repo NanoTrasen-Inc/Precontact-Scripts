f50bbf1d93e812260575d3392238d787b40af001
NanoTrasen-Inc <nano@nanotrasen.org>
2023-04-03 23:49:18 -0500

    Update import scripts and LSO bot

--- LSO Bot/Start (sample).bat
+++ LSO Bot/Start (sample).bat
@@ -7,7 +7,8 @@ set script_dir=%~dp0
 set script_dir=%script_dir:~0,-1%
 
 taskkill /f /im lso.exe
+del /f /q "%temp%\LSO-*"
 
-start "LSO Bot #1" /d "%script_dir%" /min /belownormal "%script_dir%\lso.exe" run -o gen --uri http://127.0.0.1:50011 --discord-webhook <Discord webhook URL>
-start "LSO Bot #2" /d "%script_dir%" /min /belownormal "%script_dir%\lso.exe" run -o gen --uri http://127.0.0.1:50021 --discord-webhook <Discord webhook URL>
-start "LSO Bot #3" /d "%script_dir%" /min /belownormal "%script_dir%\lso.exe" run -o gen --uri http://127.0.0.1:50031 --discord-webhook <Discord webhook URL>
+start "LSO Bot #1" /d "%script_dir%" /min /belownormal "%script_dir%\lso.exe" run -o "%temp%" --uri http://127.0.0.1:50011 --discord-webhook <Discord webhook URL>
+start "LSO Bot #2" /d "%script_dir%" /min /belownormal "%script_dir%\lso.exe" run -o "%temp%" --uri http://127.0.0.1:50021 --discord-webhook <Discord webhook URL>
+start "LSO Bot #3" /d "%script_dir%" /min /belownormal "%script_dir%\lso.exe" run -o "%temp%" --uri http://127.0.0.1:50031 --discord-webhook <Discord webhook URL>
--- Sync Tacview Files/Sync.bat
+++ Sync Tacview Files/Sync.bat
@@ -1,10 +1,5 @@
 @echo off
-REM robocopy "C:\Users\server\Documents\Tacview\Server 1" "T:\Server 1" *.zip.acmi /mir /NFL /NDL /np /compress /min:1 > nul
-REM robocopy "C:\Users\server\Documents\Tacview\Server 2" "T:\Server 2" *.zip.acmi /mir /NFL /NDL /np /compress /min:1 > nul
-REM robocopy "V:\Users\server\Documents\Tacview\Server 3" "T:\Server 3" *.zip.acmi /mir /NFL /NDL /np /compress /min:1 > nul
-REM robocopy "V:\Users\server\Documents\Tacview\Server 4" "T:\Server 4" *.zip.acmi /mir /NFL /NDL /np /compress /min:1 > nul
-REM robocopy "V:\Users\server\Documents\Tacview\Server 5" "T:\Server 5" *.zip.acmi /mir /NFL /NDL /np /compress /min:1 > nul
-REM robocopy "V:\Users\server\Documents\Tacview\Server 6" "T:\Server 6" *.zip.acmi /mir /NFL /NDL /np /compress /min:1 > nul
+echo Syncing Tacview files...
 robocopy "V:\Users\server\Documents\Tacview\Server 3" "C:\Users\server\Documents\Tacview\Server 3" *.acmi /mir /NFL /NDL /np /compress > nul
 robocopy "V:\Users\server\Documents\Tacview\Server 4" "C:\Users\server\Documents\Tacview\Server 4" *.acmi /mir /NFL /NDL /np /compress > nul
 robocopy "V:\Users\server\Documents\Tacview\Server 5" "C:\Users\server\Documents\Tacview\Server 5" *.acmi /mir /NFL /NDL /np /compress > nul
