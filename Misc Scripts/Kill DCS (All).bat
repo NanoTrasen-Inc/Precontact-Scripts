fa8e1ceeb13dcd2d07cf5be8a310b2f7f50bee9c
NanoTrasen-Inc <nano@nanotrasen.org>
2023-04-23 14:04:16 -0500

    Send output to null

--- Misc Scripts/Kill DCS (All).bat
+++ Misc Scripts/Kill DCS (All).bat
@@ -7,13 +7,13 @@ echo DCS has been killed
 taskkill /f /im "SR-ClientRadio.exe" > nul 2>&1
 echo SRS has been killed
 :: Kill TrackIR
-taskkill /f /im "TrackIR5.exe"
+taskkill /f /im "TrackIR5.exe" > nul 2>&1
 echo TrackIR5 has been killed
 :: Kill SimAppPro
-taskkill /f /im "SimAppPro.exe"
+taskkill /f /im "SimAppPro.exe" > nul 2>&1
 echo SimAppPro has been killed
 :: Kill VoiceAttack
-taskkill /f /im "VoiceAttack.exe"
+taskkill /f /im "VoiceAttack.exe" > nul 2>&1
 echo VoiceAttack has been killed
 
 
