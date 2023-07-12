7ff398cbe0650f0828cbda62702c6bacaa670868
NanoTrasen-Inc <nano@nanotrasen.org>
2023-07-12 06:56:33 -0500

    Change tacview path

--- Sync Tacview Files/Sync.bat
+++ Sync Tacview Files/Sync.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Syncing Tacview files...
-robocopy "V:\Users\server\Documents\Tacview\Server 3" "C:\Users\server\Documents\Tacview\Server 3" *.acmi /mir /NFL /NDL /np /compress > nul
-robocopy "V:\Users\server\Documents\Tacview\Server 4" "C:\Users\server\Documents\Tacview\Server 4" *.acmi /mir /NFL /NDL /np /compress > nul
-robocopy "V:\Users\server\Documents\Tacview\Server 5" "C:\Users\server\Documents\Tacview\Server 5" *.acmi /mir /NFL /NDL /np /compress > nul
-robocopy "V:\Users\server\Documents\Tacview\Server 6" "C:\Users\server\Documents\Tacview\Server 6" *.acmi /mir /NFL /NDL /np /compress > nul
+robocopy "V:\Users\server\Documents\Tacview\Server 3" "D:\Tacview\Server 3" *.acmi /mir /NFL /NDL /np /compress > nul
+robocopy "V:\Users\server\Documents\Tacview\Server 4" "D:\Tacview\Server 4" *.acmi /mir /NFL /NDL /np /compress > nul
+robocopy "V:\Users\server\Documents\Tacview\Server 5" "D:\Tacview\Server 5" *.acmi /mir /NFL /NDL /np /compress > nul
+robocopy "V:\Users\server\Documents\Tacview\Server 6" "D:\Tacview\Server 6" *.acmi /mir /NFL /NDL /np /compress > nul
