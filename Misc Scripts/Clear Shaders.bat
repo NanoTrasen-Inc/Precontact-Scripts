bcd436906b7024ffaffa5bd2be060f4319642416
NanoTrasen-Inc <nano@nanotrasen.org>
2023-12-15 15:35:51 -0600

    Close dcs in clear shaders

--- Misc Scripts/Clear Shaders.bat
+++ Misc Scripts/Clear Shaders.bat
@@ -5,6 +5,12 @@ setlocal EnableDelayedExpansion
 
 set dcs_data_dir=%USERPROFILE%\Saved Games\DCS.openbeta
 
+:: Kill DCS
+taskkill /f /im "dcs.exe" > nul 2>&1
+echo DCS has been killed
+
+:: Clear shaders
 rmdir /s /q "%dcs_data_dir%\fxo" "%dcs_data_dir%\metashaders2"
+echo Shaders have been cleared
 
 timeout 2
